f1c1cd5dcafd3e41b5d22d24ad4490a0a6258159
changha <mentenseoul@gmail.com>
2025-11-27 19:55:30 +0900

    chore : youtube controller 임시 Profile설정

--- src/main/java/com/toy/nar/api/v1/YoutubeController.java
+++ src/main/java/com/toy/nar/api/v1/YoutubeController.java
@@ -2,6 +2,7 @@ package com.toy.nar.api.v1;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.toy.nar.app.youtube.dto.YoutubeVideoDto;
 
 import lombok.RequiredArgsConstructor;
 
+@Profile({"local", "dev"})
 @RestController
 @RequiredArgsConstructor
 public class YoutubeController {
