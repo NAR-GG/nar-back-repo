f36898bbed58684fdcde9a0c6d163840d0c04517
changha <mentenseoul@gmail.com>
2026-01-22 13:39:28 +0900

    chore : /api/home/champion/top5 champions -> topPicks 필드 변경

--- src/main/java/com/toy/nar/app/analysis/dto/ChampionAnalysisResponse.java
+++ src/main/java/com/toy/nar/app/analysis/dto/ChampionAnalysisResponse.java
@@ -11,6 +11,6 @@ import java.util.List;
 public class ChampionAnalysisResponse {
 	private String patchVersion;
 	private String seasonInfo; // 예: "2024 LCK Spring" 등
-	private List<ChampionStatsDto> champions; // 픽률/승률 TOP 5
+	private List<ChampionStatsDto> topPicks; // 픽률/승률 TOP 5
 	private List<ChampionBanStatsDto> topBans; // 밴률 TOP 5
 }
