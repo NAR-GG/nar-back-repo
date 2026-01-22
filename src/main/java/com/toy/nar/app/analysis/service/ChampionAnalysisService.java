c91d0ec68ef95c9bb2623ae4e1a962a1d713d578
changha <mentenseoul@gmail.com>
2026-01-22 13:43:52 +0900

    chore : /api/home/champion/top5 champions -> topPicks 필드 변경

--- src/main/java/com/toy/nar/app/analysis/service/ChampionAnalysisService.java
+++ src/main/java/com/toy/nar/app/analysis/service/ChampionAnalysisService.java
@@ -29,11 +29,11 @@ public class ChampionAnalysisService {
 		String latestPatch = gameRepository.findLatestPatchByLeague(targetLeague);
 		if (latestPatch == null) {
 			return ChampionAnalysisResponse.builder()
-				.patchVersion("N/A")
-				.seasonInfo("N/A")
-				.champions(List.of())
-				.topBans(List.of())
-				.build();
+					.patchVersion("N/A")
+					.seasonInfo("N/A")
+					.topPicks(List.of())
+					.topBans(List.of())
+					.build();
 		}
 
 		// 2. 연도 정보 조회
@@ -41,31 +41,34 @@ public class ChampionAnalysisService {
 		String year = years.isEmpty() ? "" : String.valueOf(years.get(0));
 
 		// 3. 픽률/승률 통계 TOP 5 조회
-		List<ChampionStatsDto> pickStats = gameRepository.findChampionStatsByPatchAndLeague(latestPatch, targetLeague, PageRequest.of(0, 5));
+		List<ChampionStatsDto> pickStats = gameRepository.findChampionStatsByPatchAndLeague(latestPatch, targetLeague,
+				PageRequest.of(0, 5));
 
 		// 4. 밴률 통계 TOP 5 조회
-		List<ChampionBanStatsDto> top5Bans = gameRepository.findChampionBanStatsByPatchAndLeague(latestPatch, targetLeague)
-			.stream().limit(5).collect(Collectors.toList());
-			
+		List<ChampionBanStatsDto> top5Bans = gameRepository
+				.findChampionBanStatsByPatchAndLeague(latestPatch, targetLeague)
+				.stream().limit(5).collect(Collectors.toList());
+
 		// 5. 밴된 챔피언들의 픽/승률 통계 조회
 		if (!top5Bans.isEmpty()) {
 			List<String> bannedChampionNames = top5Bans.stream()
-				.map(ChampionBanStatsDto::getChampionNameKr)
-				.toList();
-			
-			Map<String, ChampionStatsDto> pickWinStatsMap = gameRepository.findChampionStatsByNamesAndPatch(latestPatch, targetLeague, bannedChampionNames)
-				.stream()
-				.collect(Collectors.toMap(ChampionStatsDto::getChampionNameKr, stats -> stats));
-			
+					.map(ChampionBanStatsDto::getChampionNameKr)
+					.toList();
+
+			Map<String, ChampionStatsDto> pickWinStatsMap = gameRepository
+					.findChampionStatsByNamesAndPatch(latestPatch, targetLeague, bannedChampionNames)
+					.stream()
+					.collect(Collectors.toMap(ChampionStatsDto::getChampionNameKr, stats -> stats));
+
 			// 6. 데이터 조합
 			top5Bans.forEach(banStat -> banStat.setPickWinStats(pickWinStatsMap.get(banStat.getChampionNameKr())));
 		}
 
 		return ChampionAnalysisResponse.builder()
-			.patchVersion(latestPatch)
-			.seasonInfo(year)
-			.champions(pickStats)
-			.topBans(top5Bans)
-			.build();
+				.patchVersion(latestPatch)
+				.seasonInfo(year)
+				.topPicks(pickStats)
+				.topBans(top5Bans)
+				.build();
 	}
 }
